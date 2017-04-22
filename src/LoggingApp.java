339e255dd2056041ba2fda300b5e2ffb27ed396a
KhudovekovRoman <Nuadire@gmail.com>
2017-04-22 21:05:22 +0500

    Create

---
+++ src/LoggingApp.java
@@ -0,0 +1,24 @@
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.StreamHandler;
+
+public class LoggingApp {
+	static Logger log = Logger.getLogger(LoggingApp.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 SimpleFormatter fmt = new SimpleFormatter();
+		 StreamHandler sh = new StreamHandler(System.out, fmt);
+		 sh.setLevel(Level.FINE);
+		 sh.setFormatter(new SimpleFormatter());
+		 log.addHandler(sh);
+		
+		log.log(Level.INFO, "Application started");
+
+
+	}
+
+}
